84b34e43e327ccb93b654d100bf19952665cfa6a
nathan bonnet <nathanbonnet@macbook-pro-de-nathan.home>
2021-01-18 10:55:10 +0100

    actualisation du fichier sql

--- test_projet-7.sql
+++ test_projet-7.sql
@@ -1,65 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 4.9.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Dec 28, 2020 at 02:32 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema groupomania
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema groupomania
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `groupomania` DEFAULT CHARACTER SET utf8 ;
+USE `groupomania` ;
+
+-- -----------------------------------------------------
+-- Table `groupomania`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `groupomania`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `groupomania`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(255) NULL,
+  `lastName` VARCHAR(255) NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `isAdmin` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `groupomania`.`articles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `groupomania`.`articles` ;
 
---
--- Database: `test_projet-7`
---
+CREATE TABLE IF NOT EXISTS `groupomania`.`articles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(120) NOT NULL,
+  `content` LONGTEXT NOT NULL,
+  `users_id` INT NOT NULL,
+  `image` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_articles_users_idx` (`users_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  CONSTRAINT `fk_articles_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `groupomania`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `customers`
---
+-- -----------------------------------------------------
+-- Table `groupomania`.`sharings`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `groupomania`.`sharings` ;
 
-CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `active` tinyint(1) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `groupomania`.`sharings` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `users_id` INT NOT NULL,
+  `articles_id` INT NOT NULL,
+  `content` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`, `users_id`, `articles_id`),
+  INDEX `fk_users_has_articles_articles1_idx` (`articles_id` ASC) VISIBLE,
+  INDEX `fk_users_has_articles_users1_idx` (`users_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_has_articles_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `groupomania`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_articles_articles1`
+    FOREIGN KEY (`articles_id`)
+    REFERENCES `groupomania`.`articles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Dumping data for table `customers`
---
 
-INSERT INTO `customers` (`id`, `email`, `name`, `active`) VALUES
-(1, 'nath@hotmail.fr', 'nathan', 0);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
